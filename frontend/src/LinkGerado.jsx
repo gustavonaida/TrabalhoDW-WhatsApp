d1f74bf39fcef522221026c8b043d21957f8ebac
EmanoJava <manuzin081922@gmail.com>
2025-09-26 11:16:41 -0300

    Mudei um pouco

--- frontend/src/LinkGerado.jsx
+++ frontend/src/LinkGerado.jsx
@@ -1,7 +1,9 @@
+import styles from "./LinkGerado.module.css"
+
 export default function LinkGerado(){
     return(
     <>
-    
+    <p className={styles.tittle}>Links Gerados:</p>
     </>
     )
 }
