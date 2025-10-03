a4c6753570d6885a3a7b307b3567ca6a4be9aae3
EmanoJava <manuzin081922@gmail.com>
2025-10-03 17:02:15 -0300

    relacao de Pessoa Cor

--- frontend/src/AgendaContatos.jsx
+++ frontend/src/AgendaContatos.jsx
@@ -1,12 +1,14 @@
-import { CloudMoonRain } from "lucide-react";
 import styles from "./AgendaContatos.module.css";
 import { useState } from "react";
 
 export default function AgendaContatos() {
   const [nome, setNome] = useState("");
-
   const [contatos, setContatos] = useState([]);
   const [editIndex, setEditIndex] = useState(null);
+
+  //-------------- Mostrar cores -----------------------------
+  const [ativo, setAtivo] = useState(false);
+  const [corPadrao, setCorPadrao] = useState("#1BC257");
 
   //-------------- Formatação: Telefone ------------------------
   const [numero, setNumero] = useState("");
@@ -33,20 +35,16 @@ export default function AgendaContatos() {
   };
 
   const handlesNumero = (e) => {
-    const value = e.target.value;
-    const number1 = value.replace(/\D/g, "");
-    setNumero(number1);
-
     const onlyDigits = e.target.value.replace(/\D/g, "").slice(0, 11);
     setNumero(onlyDigits);
   };
 
-  //---------------------------------------------------
+  //------------------ Handles -----------------------------
 
   const handleSalvar = () => {
     if (!numero) return;
 
-    const novoContato = { nome, numero };
+    const novoContato = { nome, numero, cor: corPadrao };
 
     if (editIndex !== null) {
       const atualizados = [...contatos];
@@ -57,6 +55,7 @@ export default function AgendaContatos() {
       setContatos([...contatos, novoContato]);
     }
 
+    setCorPadrao("#1BC257"); // reset cor
     setNome("");
     setNumero("");
   };
@@ -64,6 +63,7 @@ export default function AgendaContatos() {
   const handleEditar = (index) => {
     setNome(contatos[index].nome);
     setNumero(contatos[index].numero);
+    setCorPadrao(contatos[index].cor);
     setEditIndex(index);
   };
 
@@ -84,19 +84,22 @@ export default function AgendaContatos() {
 
       <div className={styles.doc}>
         <label>👤 Nome do contato</label>
-        <input
-          type="text"
-          className={styles.input1}
-          placeholder="Nome"
-          value={nome}
-          onChange={(e) => setNome(e.target.value)}
-        />
-          <select class={styles.selectAnimado}>
-    <option value="">Selecione</option>
-    <option value="amigo">Amigo</option>
-    <option value="familia">Família</option>
-    <option value="trabalho">Trabalho</option>
-  </select>
+        <div className={styles.NomeCor}>
+          <input
+            type="text"
+            className={`${styles.input1} ${ativo ? styles.ativo : ""}`}
+            placeholder="Nome"
+            value={nome}
+            onFocus={() => setAtivo(true)}
+            onChange={(e) => setNome(e.target.value)}
+          />
+          <input
+            type="color"
+            value={corPadrao}
+            onChange={(e) => setCorPadrao(e.target.value)}
+            className={`${styles.inputColor} ${ativo ? styles.ativo : ""}`}
+          />
+        </div>
 
         <label>📞 Número</label>
         <input
@@ -113,15 +116,21 @@ export default function AgendaContatos() {
         </button>
       </div>
 
+      <h3>Seus Contatos ({contatos.length})</h3>
       <div className={styles.listaContatos}>
-        <h3>Seus Contatos ({contatos.length})</h3>
-
         <div className={styles.AreaPessoas}>
           {contatos.map((contato, index) => (
             <div key={index} className={styles.cardContato}>
               <div className={styles.info}>
-                <strong>{contato.nome}</strong>
-                <span>{formatInputNumero(contato.numero)}</span>
+                <div
+                  className={styles.corPessoa}
+                  style={{ backgroundColor: contato.cor }}
+                ></div>
+
+                <div className={styles.infoNomeNumero}>
+                  <strong>{contato.nome}</strong>
+                  <span>{formatInputNumero(contato.numero)}</span>
+                </div>
               </div>
 
               {/* Botoes de Manipulacao */}
@@ -131,7 +140,6 @@ export default function AgendaContatos() {
                   className={styles.SendButton}
                 >
                   <strong>
-                    {" "}
                     <svg
                       xmlns="http://www.w3.org/2000/svg"
                       width="24"
@@ -139,12 +147,11 @@ export default function AgendaContatos() {
                       viewBox="0 0 24 24"
                       fill="none"
                       stroke="currentColor"
-                      stroke-width="2"
-                      stroke-linecap="round"
-                      stroke-linejoin="round"
-                      class="lucide lucide-send-icon lucide-send"
+                      strokeWidth="2"
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      className="lucide lucide-send"
                     >
-                      {"send "}
                       <path d="M14.536 21.686a.5.5 0 0 0 .937-.024l6.5-19a.496.496 0 0 0-.635-.635l-19 6.5a.5.5 0 0 0-.024.937l7.93 3.18a2 2 0 0 1 1.112 1.11z" />
                       <path d="m21.854 2.147-10.94 10.939" />
                     </svg>
@@ -162,15 +169,16 @@ export default function AgendaContatos() {
                     viewBox="0 0 24 24"
                     fill="none"
                     stroke="currentColor"
-                    stroke-width="2"
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                    class="lucide lucide-pencil-icon lucide-pencil"
+                    strokeWidth="2"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    className="lucide lucide-pencil"
                   >
                     <path d="M21.174 6.812a1 1 0 0 0-3.986-3.987L3.842 16.174a2 2 0 0 0-.5.83l-1.321 4.352a.5.5 0 0 0 .623.622l4.353-1.32a2 2 0 0 0 .83-.497z" />
                     <path d="m15 5 4 4" />
                   </svg>
                 </button>
+
                 <button
                   className={styles.deletar}
                   onClick={() => handleDeletar(index)}
@@ -182,10 +190,10 @@ export default function AgendaContatos() {
                     viewBox="0 0 24 24"
                     fill="none"
                     stroke="currentColor"
-                    stroke-width="2"
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                    class="lucide lucide-trash2-icon lucide-trash-2"
+                    strokeWidth="2"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    className="lucide lucide-trash-2"
                   >
                     <path d="M10 11v6" />
                     <path d="M14 11v6" />
