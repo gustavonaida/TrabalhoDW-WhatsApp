02de4a7669067f44ea665b0419e44a2595f28def
Stemas-VLS <manuzin081922@gmail.com>
2025-10-16 01:12:22 -0300

    quase pronto

--- frontend/src/AgendaContatos.jsx
+++ frontend/src/AgendaContatos.jsx
@@ -170,7 +170,9 @@ useEffect(() => {
                   ></div>
 
                   <div className={styles.infoNomeNumero}>
-                    <strong>{contato.nome}</strong>
+                    <strong >{contato.nome.length >15
+                      ? contato.nome.substring(0,13)+"..."
+                    : contato.nome}</strong>
                     <span>{formatInputNumero(contato.numero)}</span>
                   </div>
                 </div>
@@ -209,14 +211,14 @@ useEffect(() => {
                     onClick={() => handleEditar(index)}
                     className={styles.editButton}
                   >
-                    ✏️
+                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-pencil-icon lucide-pencil"><path d="M21.174 6.812a1 1 0 0 0-3.986-3.987L3.842 16.174a2 2 0 0 0-.5.83l-1.321 4.352a.5.5 0 0 0 .623.622l4.353-1.32a2 2 0 0 0 .83-.497z"/><path d="m15 5 4 4"/></svg>
                   </button>
 
                   <button
                     className={styles.deletar}
                     onClick={() => handleDeletar(index)}
                   >
-                    🗑️
+                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-trash2-icon lucide-trash-2"><path d="M10 11v6"/><path d="M14 11v6"/><path d="M19 6v14a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V6"/><path d="M3 6h18"/><path d="M8 6V4a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v2"/></svg>
                   </button>
                 </div>
               </div>
